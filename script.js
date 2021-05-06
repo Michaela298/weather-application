151cff348578bd361eb1abdf8664056943bc975f
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-05-06 12:26:14 +0200

    Added HTML, CSS, JS from my weather app project

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
+// Feature #1
 
+let actualDayTime = document.querySelector("#actual-day-time");
+
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+
+actualDayTime.innerHTML = `${day} ${hours}:${minutes}`;
+
+// Feature #2
+
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchLocation(position) {
+  let apiKey = "36794cb64ceccf9c27e4232061a9201a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "36794cb64ceccf9c27e4232061a9201a";
+  let city = document.querySelector("#search-text").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
